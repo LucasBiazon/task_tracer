16d0228d3e5b2bcf6dcf7be3a8eb7cfbe3e945b6
Lucas <lucasbiazonpalma@gmail.com>
2024-10-06 00:29:12 -0300

    Repository: task

---
+++ internal/entities/task.go
@@ -0,0 +1,41 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+const (
+	StatusTodo       = "todo"
+	StatusInProgress = "inprogress"
+	StatusDone       = "done"
+)
+
+type Task struct {
+	ID          string `json:"id"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
+}
+
+func NewTask(description string) *Task {
+	return &Task{
+		ID:          uuid.New().String(),
+		Description: description,
+		Status:      StatusTodo,
+		CreatedAt:   time.Now().Format("2006-01-02 15:04:05"),
+		UpdatedAt:   time.Now().Format("2006-01-02 15:04:05"),
+	}
+}
+
+type TaskRepository interface {
+	Create(Task *Task) error
+	FindAll() ([]*Task, error)
+	FindByID(id string) (*Task, error)
+	Update(id, description string) error
+	Delete(id string) error
+	MarkAsDone(id string) error
+	MarkAsInProgress(id string) error
+}
---
+++ internal/repository/task_repository.go
@@ -0,0 +1,130 @@
+package repository
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"time"
+
+	entity "github.com/lucasBiazon/task_tracker/internal/entities"
+)
+
+type TaskRepository struct {
+	filePath string
+}
+
+func NewTaskRepository(filePath string) *TaskRepository {
+	return &TaskRepository{filePath: filePath}
+}
+
+func (r *TaskRepository) loadTasks() ([]*entity.Task, error) {
+	file, err := os.OpenFile(r.filePath, os.O_RDONLY|os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	tasks := []*entity.Task{}
+	err = json.NewDecoder(file).Decode(&tasks)
+	if err != nil && err.Error() != "EOF" {
+		return nil, err
+	}
+	return tasks, nil
+}
+
+func (r *TaskRepository) saveTasks(tasks []*entity.Task) error {
+	file, err := os.OpenFile(r.filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return json.NewEncoder(file).Encode(tasks)
+}
+
+func (r *TaskRepository) Create(description string) error {
+	tasks, err := r.loadTasks()
+	if err != nil {
+		return err
+	}
+	task := entity.NewTask(description)
+	tasks = append(tasks, task)
+	return r.saveTasks(tasks)
+}
+
+func (r *TaskRepository) FindAll() ([]*entity.Task, error) {
+	return r.loadTasks()
+}
+
+func (r *TaskRepository) FindByID(id string) (*entity.Task, error) {
+	tasks, err := r.loadTasks()
+	if err != nil {
+		return nil, err
+	}
+	for _, task := range tasks {
+		if task.ID == id {
+			return task, nil
+		}
+	}
+	return nil, errors.New("tarefa não encontrada")
+}
+
+func (r *TaskRepository) Update(id, description string) error {
+	tasks, err := r.loadTasks()
+	if err != nil {
+		return err
+	}
+	for _, task := range tasks {
+		if task.ID == id {
+			task.Description = description
+			task.UpdatedAt = time.Now().Format("2006-01-02 15:04:05")
+			return r.saveTasks(tasks)
+		}
+	}
+	return errors.New("tarefa não encontrada")
+}
+
+func (r *TaskRepository) Delete(id string) error {
+	tasks, err := r.loadTasks()
+	if err != nil {
+		return err
+	}
+	for i, task := range tasks {
+		if task.ID == id {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			return r.saveTasks(tasks)
+		}
+	}
+	return errors.New("tarefa não encontrada")
+}
+
+func (r *TaskRepository) markAsStatus(id, status string) error {
+	tasks, err := r.loadTasks()
+	if err != nil {
+		return err
+	}
+	for _, task := range tasks {
+		if task.ID == id {
+			task.Status = status
+			task.UpdatedAt = time.Now().Format("2006-01-02 15:04:05")
+			return r.saveTasks(tasks)
+		}
+	}
+	return errors.New("tarefa não encontrada")
+}
+
+func (r *TaskRepository) MarkAsDone(id string) error {
+	err := r.markAsStatus(id, entity.StatusDone)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *TaskRepository) MarkAsInProgress(id string) error {
+	err := r.markAsStatus(id, entity.StatusInProgress)
+	if err != nil {
+		return err
+	}
+	return nil
+}
